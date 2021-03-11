f91708faebbf3ff9adf186769130935b403bd828
Dustin Mac <sonnyduluxwatershield@gmail.com>
2021-03-11 11:21:39 -0600

    Loops Algo Assignment

---
+++ Script/Loop Algo.js
@@ -0,0 +1,16 @@
+//1. Print odd 1 to 20
+
+for (var i = 1; i <= 20; i++) {
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+};
+
+//2. Sum and Print 1 to 5
+
+var sum = 0;
+for (var i = 1; i <= 5; i++) {
+    console.log(i);
+    sum +=i;
+    console.log(sum);
+}
